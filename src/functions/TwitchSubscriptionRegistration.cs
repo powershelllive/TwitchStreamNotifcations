f6bd679e41e1f0b61884688eb622f03a52813140
Mark Kraus <github@markekraus.com>
2019-01-21 15:12:06 -0800

    Update TwitchSubscriptionRegistration use InvokeSubscriptionRegistration

--- src/functions/TwitchSubscriptionRegistration.cs
+++ src/functions/TwitchSubscriptionRegistration.cs
@@ -20,75 +20,21 @@ namespace Markekraus.TwitchStreamNotifications
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
             HttpRequest req,
-            [Queue("%TwitchSubscribeQueue%")] IAsyncCollector<TwitchSubscription> NewSubscriptions,
-            [Queue("%TwitchUnsubscribeQueue%")] IAsyncCollector<TwitchSubscription> RemoveSubscriptions,
+            [Queue("%TwitchSubscribeQueue%", Connection = "TwitchStreamStorage")] IAsyncCollector<TwitchSubscription> SubscribeQueue,
+            [Queue("%TwitchUnsubscribeQueue%", Connection = "TwitchStreamStorage")] IAsyncCollector<TwitchSubscription> UnsubscribeQueue,
             ILogger log)
         {
-            log.LogInformation("TwitchSubscriptionRegistration processed a request.");
+            log.LogInformation($"{nameof(TwitchSubscriptionRegistration)} processed a request.");
 
             var requestBody = await req.ReadAsStringAsync();
-            log.LogInformation("RequestBody:");
-            log.LogInformation(requestBody);
+            log.LogInformation($"{nameof(TwitchSubscriptionRegistration)} RequestBody: {requestBody}");
             var TwitchSubscriptions = JsonConvert.DeserializeObject<IList<TwitchSubscription>>(requestBody);
-            log.LogInformation($"Count: {TwitchSubscriptions.Count}");
+            log.LogInformation($"{nameof(TwitchSubscriptionRegistration)} Count: {TwitchSubscriptions.Count}");
 
-            var response = new TwitchSubscriptionRegistrationResponse();
-            response.RequestSubscriptions = TwitchSubscriptions;
+            var result = await TwitchClient.InvokeSubscriptionRegistration(TwitchSubscriptions, SubscribeQueue, UnsubscribeQueue, log, nameof(TwitchSubscriptionRegistration));
 
-            log.LogInformation("Get current subscriptions");
-            var currentSubscriptions = await TwitchClient.GetTwitchWebhookSubscriptions(log);
-            response.CurrentSubscriptions = currentSubscriptions;
-
-            log.LogInformation("Create currentSubDictionary dictionary");
-            var currentSubDictionary = new Dictionary<Tuple<string, string, string>,TwitchSubscription>();
-            string currentTwittername;
-            string currentDiscordname;
-            string currentTwitchname;
-            foreach(var subscription in currentSubscriptions)
-            {
-                currentTwittername = string.IsNullOrWhiteSpace(subscription.Subscription.TwitterName) ? Utility.NameNullString : subscription.Subscription.TwitterName.ToLower();
-                currentDiscordname = string.IsNullOrWhiteSpace(subscription.Subscription.DiscordName) ? Utility.NameNullString : subscription.Subscription.DiscordName.ToLower();
-                currentTwitchname = subscription.Subscription.TwitchName.ToLower();
-                log.LogInformation($"Add TwitchName {subscription.Subscription.TwitchName} TwitterName {subscription.Subscription.TwitterName} DiscordName {subscription.Subscription.DiscordName}");
-                currentSubDictionary.Add(
-                    new Tuple<string, string, string>(currentTwitchname, currentTwittername, currentDiscordname), 
-                    subscription.Subscription);
-            }
-
-            log.LogInformation("Create requestedSubDictionary dictionary");
-            var requestedSubDictionary = new Dictionary<Tuple<string, string, string>,TwitchSubscription>();
-            foreach(var subscription in TwitchSubscriptions)
-            {
-                currentTwittername = string.IsNullOrWhiteSpace(subscription.TwitterName) ? Utility.NameNullString : subscription.TwitterName.ToLower();
-                currentDiscordname = string.IsNullOrWhiteSpace(subscription.DiscordName) ? Utility.NameNullString : subscription.DiscordName.ToLower();
-                currentTwitchname = subscription.TwitchName.ToLower();
-                log.LogInformation($"Add TwitchName {subscription.TwitchName} TwitterName {subscription.TwitterName} DiscordName {subscription.DiscordName}");
-                requestedSubDictionary.Add(
-                    new Tuple<string, string, string>(currentTwitchname, currentTwittername, currentDiscordname),
-                    subscription);
-            }
-
-            log.LogInformation("Find missing subscriptions to add");
-            response.AddSubscriptions = new List<TwitchSubscription>();
-            foreach(var missing in Enumerable.Except(requestedSubDictionary.Keys, currentSubDictionary.Keys))
-            {
-                log.LogInformation($"Add Queue TwitchName {requestedSubDictionary[missing].TwitchName} TwitterName {requestedSubDictionary[missing].TwitterName}");
-                await NewSubscriptions.AddAsync(requestedSubDictionary[missing]);
-                response.AddSubscriptions.Add(requestedSubDictionary[missing]);
-            }
-
-            log.LogInformation("Find extra subscriptions to remove");
-            response.RemoveSubscriptions = new List<TwitchSubscription>();
-            foreach(var extra in Enumerable.Except(currentSubDictionary.Keys, requestedSubDictionary.Keys))
-            {
-                log.LogInformation($"Remove Queue TwitchName {currentSubDictionary[extra].TwitchName} TwitterName {currentSubDictionary[extra].TwitterName}");
-                await RemoveSubscriptions.AddAsync(currentSubDictionary[extra]);
-                response.RemoveSubscriptions.Add(currentSubDictionary[extra]);
-
-            }
-
-            log.LogInformation("TwitchSubscriptionRegistration End");
-            var responseString = JsonConvert.SerializeObject(response);
+            log.LogInformation($"{nameof(TwitchSubscriptionRegistration)} End");
+            var responseString = JsonConvert.SerializeObject(result);
             return new OkObjectResult(responseString);
         }
     }

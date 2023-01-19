049d772f12964c8954e09a9ca0ea8562fcdfd0a0
Simon Wåhlin <simon.wahlin@advania.com>
2023-01-19 09:45:39 +0100

    Removed invalid Twitch accounts and updated registration script

--- scripts/Invoke-TwitchSubscriptionRegistration.ps1
+++ scripts/Invoke-TwitchSubscriptionRegistration.ps1
@@ -1,4 +1,4 @@
-function Invoke-TwitchSubscriptionRegistration {
+function Register-TwitchSubscription {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -13,7 +13,7 @@ function Invoke-TwitchSubscriptionRegistration {
 
         [Parameter(DontShow)]
         [string]
-        $FunctionUri = 'https://twitchstreamnotifications2.azurewebsites.net/api/TwitchSubscriptionRegistration'
+        $FunctionUri = 'https://twitchstreamnotifications2.azurewebsites.net/api/RegisterSubscription'
     )
 
     end {
@@ -34,36 +34,94 @@ function Invoke-TwitchSubscriptionRegistration {
         Invoke-RestMethod @Params
     }
 }
+function Clear-TwitchSubscription {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $FunctionSecret,
+
+        [Parameter(DontShow)]
+        [string]
+        $FunctionUri = 'https://twitchstreamnotifications2.azurewebsites.net/api/ClearSubscriptions'
+    )
+
+    end {
+        $Params = @{
+            Headers = @{
+                'x-functions-key' = $FunctionSecret
+            }
+            Uri = $FunctionUri
+            Method = 'POST'
+            ContentType = 'application/json'
+        }
+        Invoke-RestMethod @Params
+    }
+}
+function Get-TwitchSubscription {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $FunctionSecret,
+
+        [Parameter(DontShow)]
+        [string]
+        $FunctionUri = 'https://twitchstreamnotifications2.azurewebsites.net/api/GetSubscriptions'
+    )
+
+    end {
+        $Params = @{
+            Headers = @{
+                'x-functions-key' = $FunctionSecret
+            }
+            Uri = $FunctionUri
+            Method = 'GET'
+            ContentType = 'application/json'
+        }
+        Invoke-RestMethod @Params
+    }
+}
+Clear-TwitchSubscription -FunctionSecret $env:FunctionCode
+
+do {
+    Start-Sleep -Seconds 10
+    $Subscriptions = Get-TwitchSubscription -FunctionSecret $env:FunctionCode
+    $Subscriptions.data.condition.count
+} while ($Subscriptions.data.condition.count -gt 0)
 
 $invokeTwitchSubscriptionRegistrationSplat = @{
     Path = 'config/Subscriptions.json'
     FunctionSecret = $env:FunctionCode
 }
-$Result = Invoke-TwitchSubscriptionRegistration @invokeTwitchSubscriptionRegistrationSplat
-' '
-' '
-' '
-' '
-' '
-'-----------------'
-'Requested Subscriptions:'
-$Result.RequestSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
-' '
-'-----------------'
-'Current Subscriptions:'
-$Result.CurrentSubscriptions.subscription | Format-Table -AutoSize twitchname, twittername, discordname
-' '
-'-----------------'
-'Queued for Subscribe'
-$Result.AddSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
-' '
-'-----------------'
-'Queued for Unsubscribe'
-$Result.RemoveSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
-' '
-' '
-'-----------------'
-'Queued for Renewal'
-$Result.RenewSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
-' '
-' '
+Register-TwitchSubscription @invokeTwitchSubscriptionRegistrationSplat
+
+# ' '
+# ' '
+# ' '
+# ' '
+# ' '
+# '-----------------'
+# 'Requested Subscriptions:'
+# $Result.RequestSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
+# ' '
+# '-----------------'
+# 'Current Subscriptions:'
+# $Result.CurrentSubscriptions.subscription | Format-Table -AutoSize twitchname, twittername, discordname
+# ' '
+# '-----------------'
+# 'Queued for Subscribe'
+# $Result.AddSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
+# ' '
+# '-----------------'
+# 'Queued for Unsubscribe'
+# $Result.RemoveSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
+# ' '
+# ' '
+# '-----------------'
+# 'Queued for Renewal'
+# $Result.RenewSubscriptions | Format-Table -AutoSize twitchname, twittername, discordname
+# ' '
+# ' '
